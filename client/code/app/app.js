65f19b0d9b531bedd72eb62e8904218aa1299b2d
Paul Jensen <paulbjensen@gmail.com>
2013-11-23 10:42:14 +0000

    Linting

--- client/code/app/app.js
+++ client/code/app/app.js
@@ -2,40 +2,36 @@
 // Client Code
 
 
-console.log('App Loaded');
-
 var router = Davis(function () {
 
-  this.configure(function () {
-    this.generateRequestOnPageLoad = true;
-  });
+    this.configure(function () {
+        this.generateRequestOnPageLoad = true;
+    });
 
-  this.get('/', function (req) {
-    $('content').html(ss.tmpl['site-index'].r());
-  });
+    this.get('/', function () {
+        $('content').html(ss.tmpl['site-index'].r());
+    });
 
-  this.get('/getting-started', function (req) {
-    $('content').html(ss.tmpl['site-getting-started'].r());
-  });
+    this.get('/getting-started', function () {
+        $('content').html(ss.tmpl['site-getting-started'].r());
+    });
 
+    this.get('/tour', function () {
+        $('content').html(ss.tmpl['site-tour'].r());
+    });
 
-  this.get('/tour', function (req) {
-    $('content').html(ss.tmpl['site-tour'].r());
-  });
+    this.get('/news', function () {
+        $('content').html(ss.tmpl['site-news'].r());
+    });
 
-  this.get('/news', function (req) {
-    $('content').html(ss.tmpl['site-news'].r());
-  });
-
-  this.post('/search', function (req) {
-    console.log();
-    $('content').html(ss.tmpl['site-search'].render({term:req.params.term}));
-  });
+    this.post('/search', function () {
+        $('content').html(ss.tmpl['site-search'].render({term:req.params.term}));
+    });
 
 
-  this.get('/features', function (req) {
-    $('content').html(ss.tmpl['site-features'].r());
-  });
+    this.get('/features', function () {
+        $('content').html(ss.tmpl['site-features'].r());
+    });
 
 });
 
